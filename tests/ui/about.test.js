3eb89c504f74a9857e2392f07423aa34cbdf2fa3
kristianche <chelebiev777@gmail.com>
2024-06-30 15:05:39 +0300

    Fixed the UI tests

--- tests/ui/about.test.js
+++ tests/ui/about.test.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test');
 
 test('Check about page', async ({ page }) => {
   await page.goto('http://localhost:8080/about');
-  const heading = await page.$('h3');
+  const heading = await page.$('h1');
   const text = await heading.textContent();
-  expect(text).toBe('About us');
+  expect(text).toBe('About');
 });
